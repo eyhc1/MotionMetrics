8e503a3582bfa65e103e143d6363881f4092d41f
Eric Yu <yuhc218@gmail.com>
2025-05-22 13:23:09 -0700

    process data into npy

---
+++ src/python/process_files.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import pandas as pd
+from zipfile import ZipFile
+from glob import glob
+from rich import print
+
+# root = "../../data"
+root = "."
+
+labels = ["dws", "ups", "sit", "std", "wlk", "jog"]
+
+# unzip
+zip_file = ZipFile(glob(f"{root}/**/*.zip", recursive=True)[0])
+zip_file.extractall(root)
+zip_file.close()
+
+# files = glob(f"{root}/**/*dws*/*.csv", recursive=True)
+
+
+# # combine all files into one
+# frames = [pd.read_csv(file) for file in files]
+# df = pd.concat(frames, ignore_index=True, sort=False).to_csv("test.csv", index=False)
+    
+for label in labels:
+    # get all files for the label
+    files = glob(f"{root}/**/*{label}*/*.csv", recursive=True)
+    print(f"Processing {len(files)} files for label: {label}")
+
+    # combine all files into one
+    frames = [pd.read_csv(file) for file in files]
+    ds = pd.concat(frames, ignore_index=True, sort=False).to_numpy()
+    np.save(f"data/{label}.npy", ds, allow_pickle=False)
+    # df = pd.concat(frames, ignore_index=True, sort=False)
+
+    # # save to csv
+    # df.to_csv(f"{label}.csv", index=False)
+
